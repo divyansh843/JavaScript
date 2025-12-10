ec35c6e83c3099d52f625697a9a31f8e2ef2d788
Divyaansh Kumar <132772094+divyansh843@users.noreply.github.com>
2025-12-10 16:17:55 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all js code as newer version
+// alert("Hello");
+// alert(3+3); // we are using nodejs, not browser
+
+console.log(3 
+    + 3 );
+//code readebility should be high
+
+console.log(3+3);
+
+let name = "Divyansh";
+let islogin = false;
+let state = null;
+
+//primitive datatype
+// number => 2 to power 53
+// bigint
+// String => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// non-primitive
+// object
+// arrays
+
+console.log(typeof "Divyansh");
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
