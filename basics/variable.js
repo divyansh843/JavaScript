deb28f1b50bdde9d6c7d23fc2c1bfd29bf89b2db
Divyaansh Kumar <132772094+divyansh843@users.noreply.github.com>
2025-12-10 16:01:45 +0000

    variable complete

---
+++ basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "divyansh@gmail.com";
+var accountpassword = "12345";
+accountCity = "Kanpur"
+
+// accountId = 2;  Not allowed
+
+accountEmail = "dk@gmail";
+accountpassword = "12332";
+accountCity = "Kasganj";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpassword, accountCity])
+
+/*
+prefare not to use var
+becouse of issue in block scope and functional scope
+*/
