d876b172f002d854b7b204cf6760603c95b89c86
Divyaansh Kumar <132772094+divyansh843@users.noreply.github.com>
2025-12-10 17:08:47 +0000

    comparision

---
+++ basics/04_comparision.js
@@ -0,0 +1,24 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// aise convrsion nhi karne chahiye
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+// === cheack values or datatype
+console.log("2" === 2);
+
+
+
