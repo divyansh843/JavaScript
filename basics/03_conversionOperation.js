81e59039d436d8318a2ca4552df6e685e5182659
Divyaansh Kumar <132772094+divyansh843@users.noreply.github.com>
2025-12-10 17:00:16 +0000

    conversion operation

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -8,7 +8,6 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
-
 // "33" => 33
 // "33asd" => NaN not an number
 // true => 1; flase => 0
@@ -24,10 +23,41 @@ console.log(booleanIsLoggedIn);
 //"" => false
 // "Divyansh" => true
 
-
 let num = 33;
 let numString = String(num);
 console.log(numString);
 console.log(typeof(numString));
 console.log(typeof(num)); // => number
 
+//    <-------Operations--------->
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Divyansh";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+
+gameCounter++;
+console.log(gameCounter);
